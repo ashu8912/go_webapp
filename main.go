d3baca951847ccacd852ce852ffb107e18620bcf
Ashu Ghildiyal <ashu@kinvolk.io>
2020-03-26 01:10:17 +0530

    last moment code changes seperated code into different files

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+func render(c *gin.Context, data gin.H, template string) {
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+
+	case "application/xml":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, template, data)
+	}
+}
 func main() {
+
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-
 	initializeRoutes(router)
 	router.Run()
+
 }
--- routers.go
+++ routers.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
-func showIndexPage(c *gin.Context){
-c.HTML(
-			http.StatusOK,
-			"index.html",
-			gin.H{
-				"title": "Home page",
-			},
-		)
-	}
 
-func initializeRoutes(router gin.IRouter){
+func initializeRoutes(router gin.IRouter) {
 	router.GET("/", showIndexPage)
+	router.GET("/article/view/:article_id", getArticle)
 }
